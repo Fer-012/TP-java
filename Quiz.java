a0b04eb9421a31197f63409731fcebbf15954c94
Fer-012 <Fer-012>
2024-02-09 11:55:17 +0100

    Ajout du fichier VariableIntermediaire.java

---
+++ Quiz.java
@@ -0,0 +1,15 @@
+/**
+ * Quiz
+ */
+public class Quiz {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; i < 3; ++i) {
+                System.out.print(j);
+            }
+            System.out.print(" ");
+        }
+    }
+}
